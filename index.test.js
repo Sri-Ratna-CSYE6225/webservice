aaceaef0a79ec791f972a59ddf33373ee0ab22a6
Sri Ratna Akkineni <akkineni.sr@northeastern.edu>
2022-02-16 20:54:03 -0500

    Fixed test case

--- index.test.js
+++ index.test.js
@@ -1,12 +1,9 @@
-import express from 'express';
-import  request  from 'supertest';
-const app = new express();
-import router from './index';
-app.use(router);
-
-describe('Index JS routes', () => {
-    test("GET /healthz", async () => {
-        const res = await request(app).get('/healthz');
-        expect(res.statusCode).toBe(200);
-      });
+const supertest = require('supertest');
+const app = require('./index');
+describe("Index JS file", () => {
+  it("should return response code 200", (done) => 
+  {supertest(app).get("/healthz").expect(200).end((err, res) => 
+  {if (err) return done(err);return done();});
 });
+});
+
