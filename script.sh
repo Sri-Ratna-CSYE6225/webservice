9ac480d5619b407f7974ae31f6f93f93b829fd93
Sri Ratna Akkineni <akkineni.sr@northeastern.edu>
2022-03-03 14:40:39 -0500

    Added service and script files

--- script.sh
+++ script.sh
@@ -1,50 +1,25 @@
 sleep 30
-
-# sudo yum -y update
-# sudo yum -y install git
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash -y
-# . ~/.nvm/nvm.sh
-# nvm install node -y
-# git clone git://github.com/joyent/node.git ~/node
-# curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-# sudo yum -y install nodejs
-# sudo yum -y update
-# locate apt-get
-
-
 sudo yum update -y
-sudo yum install git make gcc -y
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-# source ~/.bashrc
-# nvm install 14
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash -y
-# . ~/.nvm/nvm.sh
-# nvm install node -y
-# git clone git://github.com/joyent/node.git ~/node
-# curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-# git clone git://github.com/joyent/node.git ~/node
-#         cd ~/node
-#         git checkout v0.10.37
-#         ./configure
-#         make
-#         sudo make install
-#         node -v
-#         git clone https://github.com/npm/npm.git ~/npm
-#         cd ~/npm
-#         git checkout v2.7.1
-#         sudo make install
-#         npm -v
-#         sudo npm install -g pm2 gulp nodemon forever bower n 
+sudo yum install git -y
 sudo amazon-linux-extras install epel
-# sudo yum install nodejs -y
-# npm install -g npm@latest
-# sudo chmod -R 777 /usr/local/lib
-# sudo npm install -g pm2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
+sudo yum install -y nodejs 
+sudo npm install -g pm2
 
-sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
-sudo yum -y install mysql-community-server
-sudo systemctl enable --now mysqld
-systemctl status mysqld
-mysqld -u root --password=password
+# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
+# sudo yum -y install mysql-community-server
+# sudo systemctl enable --now mysqld
+# systemctl status mysqld
+# pass=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
+# mysql -u root --password=$pass-e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'hello123'; flush privileges;" --connect-expired-password
+# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
+# sudo yum -y install mysql-community-server
+# sudo systemctl enable --now mysqld
+# systemctl status mysqld
+# passwords=$(sudo grep 'temporary password' /var/log/mysqld.log | awk {'print $13'})
+# mysql --connect-expired-password -u root -p$passwords -e \"ALTER USER 'root'@'localhost' IDENTIFIED BY 'Elgsid@1234';\"
+# mysql -u root -phello@1234 -e \"create database userdetails;\"
 mkdir /home/ec2-user/node-app
 chown ec2-user:ec2-user /home/ec2-user/node-app
--- script1.sh
+++ script1.sh
@@ -1,18 +1,26 @@
 sleep 30
 sudo yum update -y
-sudo yum install git make gcc -y
+sudo yum install git -y
 sudo amazon-linux-extras install epel
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
 sudo yum install -y nodejs 
 sudo npm install -g pm2
 
-sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
-sudo yum -y install mysql-community-server
-sudo systemctl enable --now mysqld
-systemctl status mysqld
-mysqld -u root --password=password
+# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
+# sudo yum -y install mysql-community-server
+# sudo systemctl enable --now mysqld
+# systemctl status mysqld
+# pass=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
+# mysql -u root --password=$pass-e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'hello123'; flush privileges;" --connect-expired-password
+# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
+# # sudo amazon-linux-extras install epel
+# sudo yum -y install mysql-community-server
+# sudo systemctl enable --now mysqld
+# systemctl status mysqld
+# passwords=$(sudo grep 'temporary password' /var/log/mysqld.log | awk {'print $13'})
+# mysql --connect-expired-password -u root -p$passwords -e \"ALTER USER 'root'@'localhost' IDENTIFIED BY 'Elgsid@1234';\"
+# mysql -u root -phello@1234 -e \"create database userdetails;\"
 mkdir /home/ec2-user/node-app
 chown ec2-user:ec2-user /home/ec2-user/node-app
