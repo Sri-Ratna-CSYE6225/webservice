78dea140f44032c1273afc8c90a4a02a4cac11ce
Sri Ratna Akkineni <akkineni.sr@northeastern.edu>
2022-03-30 11:21:54 -0400

    Added code deploy agent to ami and git actions

--- script1.sh
+++ script1.sh
@@ -7,20 +7,16 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
 sudo yum install -y nodejs 
 sudo npm install -g pm2
+sudo yum install ruby
+sudo yum install wget
+CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
+$CODEDEPLOY_BIN stop
+yum erase codedeploy-agent -y
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
 
-# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
-# sudo yum -y install mysql-community-server
-# sudo systemctl enable --now mysqld
-# systemctl status mysqld
-# pass=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $13}')
-# mysql -u root --password=$pass-e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'hello123'; flush privileges;" --connect-expired-password
-# sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
-# # sudo amazon-linux-extras install epel
-# sudo yum -y install mysql-community-server
-# sudo systemctl enable --now mysqld
-# systemctl status mysqld
-# passwords=$(sudo grep 'temporary password' /var/log/mysqld.log | awk {'print $13'})
-# mysql --connect-expired-password -u root -p$passwords -e \"ALTER USER 'root'@'localhost' IDENTIFIED BY 'Elgsid@1234';\"
-# mysql -u root -phello@1234 -e \"create database userdetails;\"
 mkdir /home/ec2-user/node-app
 chown ec2-user:ec2-user /home/ec2-user/node-app
