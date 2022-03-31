a6362e07a65971d2a204c85099dcabbcc76735ac
Sri Ratna Akkineni <akkineni.sr@northeastern.edu>
2022-03-30 20:33:49 -0400

    Added codedeploy folder and appspec file

---
+++ codedeploy/afterInstall.sh
@@ -0,0 +1,6 @@
+cd /home/ec2-user
+pwd 
+sudo chown ec2-user: node-app
+pwd
+cd /home/ec2-user/node-app
+npm install
---
+++ codedeploy/applicationStart.sh
@@ -0,0 +1,5 @@
+cd /home/ec2-user/webapp
+pwd
+ls
+pm2 stop all
+pm2 start app.js
