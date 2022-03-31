2c32590f6171128cd17ff549b97d07cbb299abf3
Sri Ratna Akkineni <akkineni.sr@northeastern.edu>
2022-03-31 10:53:07 -0400

    changed code deploy files

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -3,3 +3,8 @@ pwd
 ls
 pm2 stop all
 pm2 start index.js
+sudo pm2 startup systemd
+sudo pm2 save
+sudo ln -s /home/ec2-user/node-app/node-service.service /etc/systemd/system/node-service.service
+sudo systemctl daemon-reload
+sudo systemctl restart node-service.service
